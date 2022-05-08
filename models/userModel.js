4ec92d994b90ec516803e947a6d34078415f49ba
Godswilly <kalu.agukalu@gmail.com>
2022-05-08 21:17:47 +0100

    Implement user model

---
+++ models/userModel.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: [true, 'Please enter your name'],
+		minlength: [3, 'Name must have more or equal to 3 characters'],
+		maxlength: [20, 'Name must have less or equal to 20 characters'],
+	},
+	email: {
+		type: String,
+		unique: true,
+		required: [true, 'Please enter an email'],
+		lowercase: true,
+		validate: [validator.isEmail, 'Please provide a valid email address'],
+	},
+	password: {
+		type: String,
+		required: [true, 'Please provide a password'],
+		minlength: [6, 'Minimum password length must be 6 characters'],
+		select: false,
+	},
+	confirmPassword: {
+		type: String,
+		required: [true, 'Please confirm your password'],
+		validate: {
+			validator: function (el) {
+				return el === this.password;
+			},
+			message: 'Passwords are not the same!!',
+		},
+	},
+	role: {
+		type: String,
+		enum: ['user', 'admin'],
+		default: 'user',
+	},
+	active: {
+		type: Boolean,
+		default: true,
+		select: false,
+	},
+  createdAt: {
+		type: Date,
+		default: Date.now(),
+		select: false,
+	},
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
